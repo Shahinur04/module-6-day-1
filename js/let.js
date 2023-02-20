ab8181f4ee09085ace2b5ac4ddf81afbe3284394
Shahinur Islam <shahinur04042002@gmail.com>
2023-02-20 11:58:36 +0600

    let's vs const

---
+++ js/let.js
@@ -0,0 +1,11 @@
+//breakup with var
+//lets relations with let and const
+let money = 25;
+money = 35;
+console.log(money);
+
+const girlfriend='jannat';
+//hbe nh reassign change
+// girlfriend='Kristi';
+const newGf=girlfriend+'newnewnew';
+console.log(newGf);
