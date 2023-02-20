bb95b8755260d1fda7f0a71752091b85e69d8ee3
Shahinur Islam <shahinur04042002@gmail.com>
2023-02-20 23:14:53 +0600

    templets.js

---
+++ js/templete.js
@@ -0,0 +1,21 @@
+const person='Shahinur';
+const person1="jannat";
+const person3=`love`;
+// making new line 
+//const multiline=`first line text \n`+
+// `second line \n`+`third line text \n`
+const multiline=`first line text `+
+`second line `+`third line text `
+// console.log(multiline)
+const multiline1=`first line of text
+second line of string
+third line of kibba`
+// console.log(multiline1)
+const a=20;
+const b=34;
+const c=56;
+// const love=[`shahinur,jannat,riya,tanjila,nishad,tabassum`];
+// console.log(a+b+c+love);
+const array=[2,3,4,5,5,4,3,2,2,2,2,2,2,2,2,4];
+const summary=`some of; ${a}  and ${Number.length} is ${a+b}`;
+console.log(array.length)
