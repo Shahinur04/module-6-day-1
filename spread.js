933d73ba0d0f6792de9355303e3f9fcaf03f83e7
Shahinur Islam <shahinur04042002@gmail.com>
2023-02-21 13:05:50 +0600

    spread.js

---
+++ spread.js
@@ -0,0 +1,18 @@
+//find out max numbers
+const max=Math.max(23,45,76,98,9999);
+
+const min=Math.min(23,45,76,98,9999);
+// console.log(min,max)
+
+//find out largest number from array
+
+const numbers=[1,2,3,4,5,6,7,9];
+// const largest=Math.max(...numbers);
+// console.log(largest)
+
+numbers.push(77);
+const numbers2=numbers;
+console.log(numbers2)
+
+// const numbers1=[0,...numbers,8];
+// numbers=numbers1
